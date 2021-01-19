87b93d43aa4eb99aad200f55a71a4e7628ba5cb6
steffilazerte <steffi@steffi.ca>
2021-01-19 17:06:01 -0600

    Revert "Update to BirdCave workshop"
    This reverts commit 806ef60d241d09711ad0a56f860e01fd1a273a00.

--- _rmd/0_RUN.R
+++ _rmd/0_RUN.R
@@ -1,65 +1,65 @@
 library(tidyverse)
 library(glue)
 
-#remotes::update_packages(c("pagedown", "xaringan"))
-#remotes::update_packages()
-
-#Params
-p <- list(name = "Bird Cave Workshop 2021")
-
-files <- tribble(~f,                                   ~nice,
-                 "intro_to_r.Rmd",                   "Intro to R through Figures") %>%
-  mutate(nice = paste0(p$name, " - ", nice),
-         answers = 1:n() %in% 1:4)
-
-files <- files %>%
-  filter(answers == TRUE) %>%
-  mutate(nice_file = glue("{nice} - answers")) %>%
-  bind_rows(mutate(files, nice_file = nice)) %>%
-  mutate(answers = str_detect(nice_file, "answers")) %>%
-  arrange(nice)
-
-
-p <- glue("{names(p)} = '{p}'") %>%
-  glue_collapse(sep = ", ")
-
-# Check spelling
-for(i in seq_len(nrow(files))) {
-  
-  setwd("_rmd/")
-  
-  # Clean Cache before each run
-  unlink(list.files(pattern = "_cache", full.names = TRUE), recursive = TRUE)
-  
-  # Render HTML
-  glue("Rscript -e ",
-       "\"rmarkdown::render('{files$f[i]}', ",
-       "output_file = '{files$nice_file[i]}.html', ",
-       "params = list({p}, hide_answers = {!files$answers[i]}))\"") %>%
-    system()
-  
-  # Create PDFS
-  message("Save as PDF")
-  pagedown::chrome_print(glue("{files$nice_file[i]}.html"),
-                         glue("{files$nice_file[i]}.pdf"),
-                         extra_args = "--font-render-hinting=none")
-  
-  # Reduce size (prepress = 300 dpi, printer = 300 dpi, ebook = 150 dpi, screen = 72dpi)
-  message("Make Small")
-  system(glue("gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 ",
-              "-dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH ",
-              "-sOutputFile='./Handouts/{files$nice_file[i]}.pdf' ",
-              "'{files$nice_file[i]}.pdf'"))
-  
-  # Make Handouts
-  message("Save as Handout")
-  # From https://github.com/DavidFirth/pdfjam-extras/blob/master/bin/pdfjam-slides3up
-  system(glue("pdfjam --nup 1x3 --frame true --noautoscale false --delta '0cm 0.2cm' ",
-              "--scale 0.87 --offset '-2.8cm 0cm' ",
-              "--preamble '\\footskip 3.1cm' --pagecommand '\\thispagestyle{{plain}}' ",
-              "--outfile './Handouts/' --suffix 'print' ",
-              "'./Handouts/{files$nice_file[i]}.pdf'"))
-  
-  unlink(list.files(pattern = "_cache", full.names = TRUE), recursive = TRUE)
-  setwd("../")
+detach_package <- function(pkg, character.only = FALSE)
+{
+  if(!character.only)
+  {
+    pkg <- deparse(substitute(pkg))
+  }
+  search_item <- paste("package", pkg, sep = ":")
+  while(search_item %in% search())
+  {
+    detach(search_item, unload = TRUE, character.only = TRUE)
+  }
 }
+
+if(!dir.exists("./_rmd/Handouts")) dir.create("./_rmd/Handouts")
+
+f <- "_rmd/tws_intro_to_r.Rmd"
+nice <- "TWS - Intro to R through Figures 2020"
+
+if(file.exists(f)) {
+  # Clean Cache
+  basename(f) %>%
+    str_remove(".Rmd") %>%
+    glue("_cache") %>%
+    unlink()
+  
+  system(glue("Rscript -e ",
+              "'rmarkdown::render(\"{f}\", ",
+              "output_file = \"{nice} - answers.html\", ",
+              "params = list(hide_answers = FALSE))'"))
+  
+  system(glue("Rscript -e ",
+              "'rmarkdown::render(\"{f}\", ",
+              "output_file = \"{nice}.html\", ",
+              "params = list(hide_answers = TRUE))'"))
+  
+  for(a in c(" - answers", "")) {
+    
+    message("Save as PDF")
+    pagedown::chrome_print(glue("./_rmd/{nice}{a}.html"),
+                           glue("./_rmd/{nice}{a}.pdf"))
+    
+    # Reduce size (prepress = 300 dpi, printer = 300 dpi, ebook = 150 dpi, screen = 72dpi)
+    message("Make Small")
+    system(glue("gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 ",
+                "-dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH ",
+                "-sOutputFile='./_rmd/Handouts/{nice}{a}.pdf' ",
+                "'./_rmd/{nice}{a}.pdf'"))
+    
+    # Make Handouts
+    message("Save as Handout")
+    # From https://github.com/DavidFirth/pdfjam-extras/blob/master/bin/pdfjam-slides3up
+    system(glue("pdfjam --nup 1x3 --frame true --noautoscale false --delta '0cm 0.2cm' ",
+                "--scale 0.87 --offset '-2.8cm 0cm' ",
+                "--preamble '\\footskip 3.1cm' --pagecommand '\\thispagestyle{{plain}}' ",
+                "--outfile './_rmd/Handouts/' --suffix 'print' ",
+                "'./_rmd/Handouts/{nice}{a}.pdf'"))
+
+  }
+  unlink(list.files("./_rmd/", "_cache", full.names = TRUE), recursive = TRUE)
+}
+
+
